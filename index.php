648f143e8de0e5c9db49f78617597fcf95c21d59
omar.yerden <omaryer@hotmail.com>
2015-12-29 22:51:38 -0300

    First commit for recomendados.com

---
+++ index.php
@@ -0,0 +1,76 @@
+<head>
+	<title>Undefined - Work in Progress</title>
+
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="css/undefined.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+</head>
+<body>
+	<div class="level" id="home">
+		<div class="slide-background">
+			<ul>
+				<li id="bg-plumber"><img src="images/slide-background/plumbing.jpg" width="100%"></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+		<div class="header">
+			<div class="container">
+				<h1 class="logo-header"><a href="#home"><img src="images/logo-undefined-335.png" alt="UNDEFINED"></a></h1>
+				<nav>
+					<ul class="menu-header">
+						<li id="home"><a href="#home" class="current">Inicio</a></li>
+						<li id="home"><a href="#tour">Como Funciona?</a></li>
+						<li id="home"><a href="#favorite">Mi Agenda</a></li>
+						<li id="home"><a href="#contact">Contactanos</a></li>
+					</ul>
+				</nav>
+				<div class="clearFix"></div>
+				<hr class="header-border">
+			</div>
+
+			<div class="container">
+				<section class="slogan-boxes aligncenter">
+					<div class="slogan">
+						<h2>Y VOS DE QUE QUERES!</h2>
+						<p class="text-01">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed dolorem aut consequatur in nesciunt perspiciatis maiores facere, molestiae temporibus atque recusandae officia soluta at quidem eligendi qui voluptates, excepturi reiciendis.</p>
+					</div>
+
+					<form>
+						<ul class="steps begin">
+							<li>
+								<span>Que estas buscando?</span>
+								<select class="fullwidth">
+									<option>Plomero</option>
+								</select>
+							</li>
+							<li>
+								<span>Para que lo necesitas</span>
+								<select>
+									<option>Cambiar un cuerito</option>
+								</select>
+							</li>
+							<li>
+								<span>Para cuando y Donde</span>
+
+							</li>
+						</ul>
+					</form>
+
+					<div class="clearfix"></div>
+					<a class="btn-01 def-btn btn-begin" href="#">Comenzar</a>
+				</section>
+			</div>
+		</div>
+	</div>
+</body>
+
+<script src="add-ons/javascript/jquery/1.11.3/jquery.min.js"></script>
+<script src="add-ons/javascript/jquery_ui/jquery-ui.min.js"></script>
+<script src="add-ons/javascript/bootsrap/3.3.6/bootstrap.min.js"></script>
